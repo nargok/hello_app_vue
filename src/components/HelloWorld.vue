6032d26af4c589665d05e8fc633eae3d5be9dbd5
nargok <nargok@gmail.com>
2019-03-16 22:13:18 +0900

    v-on inputしたテキストを表示する

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,11 @@
   <div class="hello">
     <h1>{{ title }}</h1>
     <p>{{ message }}</p>
+    <hr>
+    <div>
+      <input type="text" v-model="input">
+      <button v-on:click="doAction()">Click</button>
+    </div>
   </div>
 </template>
 
@@ -10,7 +15,17 @@ export default {
   name: 'HelloWorld',
   props: {
     title: String,
-    message: String,
+  },
+  data: function () {
+    return {
+      message: 'お名前は？',
+      input: 'no name',
+    };
+  },
+  methods: {
+    doAction: function () {
+      this.message = 'こんにちは、 ' + this.input + ' さん';
+    }
   }
 }
 </script>
